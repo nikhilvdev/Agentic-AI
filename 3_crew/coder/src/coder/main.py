13c807537e67cfe37dda9e06b319ba7666724746
NiKhil AroRa <nikhil290192@gmail.com>
2025-09-21 20:25:55 +0530

    Started with the CrewAI

---
+++ 3_crew/coder/src/coder/main.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import sys
+import warnings
+import os
+
+from datetime import datetime
+
+from coder.crew import Coder
+
+warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
+
+# This main file is intended to be a way for you to run your
+# crew locally, so refrain from adding unnecessary logic into this file.
+# Replace with inputs you want to test with, it will automatically
+# interpolate any tasks and agents information
+
+os.makedirs("output", exist_ok=True)
+
+assignment = 'Write a python program to calculate the first 10,000 terms \
+    of this series, multiplying the total by 4: 1 - 1/3 + 1/5 - 1/7 + ...'
+
+def run():
+    """
+    Run the crew.
+    """
+    inputs = {
+        'assignment': assignment,
+    }
+
+    result = Coder().crew().kickoff(inputs=inputs)
+    print(result.raw)
+
